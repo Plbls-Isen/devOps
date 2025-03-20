64dcc6664771d654e3e89302248fe205ac5dbe45
Julien Jaffres <jaffres.julien6@gmail.com>
2025-03-20 16:26:29 +0100

    Add API Gateway configuration for Lambda function

--- main.tf
+++ main.tf
@@ -64,3 +64,52 @@ output "lambda_url" {
   value       = aws_lambda_function_url.lambda_url.function_url
 }
 
+
+
+
+
+resource "aws_api_gateway_rest_api" "lambda_api" {
+  name        = "LambdaAPIg11"
+  description = "API Gateway for Lambda function"
+}
+
+resource "aws_api_gateway_resource" "lambda_resource" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  parent_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
+  path_part   = "hello"
+}
+
+resource "aws_api_gateway_method" "lambda_get" {
+  rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
+  resource_id   = aws_api_gateway_resource.lambda_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.lambda_api.id
+  resource_id             = aws_api_gateway_resource. lambda_resource.id
+  http_method             = aws_api_gateway_method.lambda_get.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.hour_name.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hour_name.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.lambda_api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "lambda_deployment" {
+  depends_on  = [aws_api_gateway_integration.lambda_integration]
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  stage_name  = "prod"
+}
+
+output "api_url" {
+  description = "L'URL pour accéder à l'API Gateway"
+  value       = "${aws_api_gateway_deployment.lambda_deployment.invoke_url}/hello"
+}
